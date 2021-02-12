6cf2fc4afd05e760c6a9a589228c70b80e27918a
Stephanie <stephanie.enciso13@gmail.com>
2021-02-12 05:05:46 -0600

    Finished all the actions-router endpoints.

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -27,4 +27,43 @@ router.get('/:id', validateActionId, (req, res) => {
     res.status(200).json(req.action);
 });
 
+router.post('/:id', validateProjectId, validateAction, (req, res) => {
+    const actionInfo = {...req.body, project_id: req.params.id}
+    Action.insert(actionInfo)
+      .then(action => {
+          res.status(201).json(actionInfo)
+      })
+      .catch(error => {
+          console.log(error);
+          res.status(500).json({
+              message: 'The action could not be added.'
+          });
+      });
+});
+
+router.put('/:id', validateActionId, validateAction, (req, res) => {
+    Action.update(req.params.id, req.body)
+      .then(action => {
+          res.status(200).json(action);
+      })
+      .catch(error => {
+          res.status(500).json({
+              message: 'The action could not be updated.'
+          });
+      });
+});
+
+router.delete('/:id', validateActionId, (req, res) => {
+    Action.remove(req.params.id)
+      .then(action => {
+          res.status(200).json(action);
+      })
+      .catch(error => {
+          console.log(error);
+          res.status(500).json({
+              message: 'Action could not be removed.'
+          })
+      })
+})
+
 module.exports = router;
